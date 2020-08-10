15e6200fed5390181a8fccb5c0d3d34c03503640
Kinnari Shah <kinnari1022@yahoo.com>
2020-08-10 16:08:24 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -9,11 +9,10 @@ module Memorable
   def count
     all.count
   end
-
 end
-module InstanceMethods
-  def initialize 
-    self.class.all << self 
-  end 
-end
+  module InstanceMethods
+    def initialize 
+      self.class.all << self 
+    end 
+  end
 end
