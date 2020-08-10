a24d51e26c4faa2bb92bbcf1d4bd60d5f6530073
Kinnari Shah <kinnari1022@yahoo.com>
2020-08-10 15:48:00 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -20,21 +20,9 @@ class Artist
     Song.all.select {|s| s.artist == self}
   end
   
-  # def self.find_by_name(name)
-  #   @@artists.detect{|a| a.name == name}
-  # end
-
   def self.all
     @@all
   end
-
-  # def self.reset_all
-  #   self.all.clear
-  # end
-
-  # def self.count
-  #   self.all.count
-  # end
 
   def add_song(song)
     #@songs << song
