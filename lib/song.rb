0245d3db37c5b1301f8055d22e9da69dcebba519
Kinnari Shah <kinnari1022@yahoo.com>
2020-08-10 15:46:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,19 +3,20 @@ require_relative '../lib/concerns/memorable.rb'
 
 class Song
   extend Memorable::ClassMethods
+  extend Findable::ClassMethods
  
   attr_accessor :name
   attr_reader :artist
 
-  @@songs = []
+  @@all = []
 
   def initialize
-    @@songs << self
+    @@all << self
   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  # def find_by_name(name)
+  #   self.all.detect{|a| a.name == name}
+  #end
 
   def self.all
     @@songs
