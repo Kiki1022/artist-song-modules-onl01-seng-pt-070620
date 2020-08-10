43154aecf443691977b4378d284351184d63368c
Kinnari Shah <kinnari1022@yahoo.com>
2020-08-10 15:56:01 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,8 @@
 require 'pry'
-require_relative '../lib/concerns/memorable.rb'
-require_relative '../lib/concerns/findable.rb'
+require_relative '../lib/concerns/findable'
+require_relative '../lib/concerns/memorable'
+require_relative '../lib/concerns/paramable'
+
 
 class Artist
  extend Findable::ClassMethods
