ff606c4f08b9e7e64c2dac558542cc394985928c
Kinnari Shah <kinnari1022@yahoo.com>
2020-08-10 15:47:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,21 +14,9 @@ class Song
     @@all << self
   end
 
-  # def find_by_name(name)
-  #   self.all.detect{|a| a.name == name}
-  #end
-
   def self.all
     @@all
   end
-
-  # def self.reset_all
-  #   self.all.clear
-  # end
-
-  # def self.count
-  #   self.all.count
-  # end
 
   def artist=(artist)
     @artist = artist
